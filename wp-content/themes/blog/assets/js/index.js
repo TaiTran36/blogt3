261b26c1c9a86489cc5b8331e848d0ad773a221e
taiitt <taitt@chainos.vn>
2021-11-30 23:03:54 +0700

    add files

---
+++ wp-content/themes/blog/assets/js/index.js
@@ -0,0 +1,45 @@
+jQuery(document).ready(function () {
+    scroll_auto(2000);
+
+    $(window).resize(function() {
+        var windowsize = $(window).width();
+        if(windowsize > 509 && windowsize < 991){
+            $('#site_right').css("width",'70%');
+        }
+        else if(windowsize < 509){
+            $('#site_right').css("width",'85%');
+        }
+        else{
+            $('#site_right').css("width",'60%');
+        }
+    });
+
+    $('.menu .list_item').click(function(){
+        $('#site_right').animate({
+            width:'0',
+        },500);
+        scroll_auto(0);
+    });
+})
+
+function start_display_info() {
+    if($('#site_right').hasClass('start_theme')) {
+        $('#site_right').removeClass('start_theme')
+        $('.height_100').css('justify-content' , 'left');
+    }
+}
+
+function scroll_auto(delay) {
+    $('#site_right').delay( delay ).animate({
+        width: '60%',
+    },500, function(){
+        let windowsize = $(window).width();
+        let width = "60%";
+        if(windowsize < 509){
+            width = "80%";
+        }
+
+        $('#site_right').css('width' , width);
+        start_display_info()
+    });
+}
---
+++ wp-content/themes/blog/functions.php
@@ -0,0 +1,68 @@
+<?php
+function script_load()
+{
+    $my_ver  = "20211115094500";
+    wp_enqueue_script('jquery-validate', get_stylesheet_directory_uri() . '/assets/js/vendor/jquery.validate.min.js', array('jquery'));
+    wp_enqueue_script('index', get_stylesheet_directory_uri() . '/assets/js/index.js', array('jquery'), $my_ver);
+
+    wp_localize_script('index', 'myAjax', array('ajaxurl' => admin_url('admin-ajax.php')));
+    wp_localize_script('index', 'dataCommon', array('templateDirectoryUri' => get_template_directory_uri()));
+}
+
+add_action('wp_enqueue_scripts', 'script_load');
+
+add_action('wp_head', 'my_custom_styles', 100);
+
+function my_custom_styles()
+{
+    $color =  esc_attr(get_option( 'color_theme_pick', true));
+    echo "<style>
+            .site_right {
+                background-color: " . $color . " !important;
+            }
+            
+            .image::before {
+                border-top-color: " . $color . " !important;
+            }
+            
+            .hexagon {
+                background-color: " . $color . " !important;
+            }
+            
+            .hexagon::before {
+                border-bottom-color: " . $color . " !important;
+            }
+            
+            .hexagon::after {
+                border-top-color: " . $color . " !important;
+            }
+            
+            .menu {
+                background-color: " . $color . " !important;
+            }
+        </style>";
+}
+
+function display_contact_social() {
+    $socials =  get_option( 'contact_socials', true);
+
+    $socials_generate = '';
+
+    if(isset($socials['fb_active']) && $socials['fb_active'] = 'on') {
+        $socials_generate .= '<li data-social="facebook"><i class="fab fa-fw fa-facebook-f social_item"></i></li>';
+    }
+
+    if(isset($socials['ins_active']) && $socials['ins_active'] = 'on') {
+        $socials_generate .= '<li data-social="instagram"><i class="fab fa-instagram social_item"></i></li>';
+    }
+
+    if(isset($socials['mail_active']) && $socials['mail_active'] = 'on') {
+        $socials_generate .= '<li data-social="mail"><i class="material-icons social_item">mail_outline</i></li>';
+    }
+
+    if(isset($socials['linkedin_active']) && $socials['linkedin_active'] = 'on') {
+        $socials_generate .= '<li data-social="mail"><i class="fab fa-linkedin social_item"></i></li>';
+    }
+
+    return $socials_generate;
+}
